5d1a3b9f5ded794a665b24b8510c43fbed55fd16
xmik <xmik2021@gmail.com>
2025-04-24 04:27:21 +0300

    fixed tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,8 +17,16 @@ def test_post_small_graph(client):
     data = "3 2\n1 3\n2 3\n"
     resp = client.post('/', data={'input_data': data})
     assert resp.status_code == 200
+
     text = resp.data.decode('utf-8')
-    # проверяем, что во вёрстке появился правильный ответ
-    assert "2" in text.splitlines()[text.splitlines().index("<h2>Результат:</h2>") + 1]
+    lines = text.splitlines()
+    # находим строку с <h2>Результат:</h2> и смотрим следующую за ней
+    idx = lines.index("<h2>Результат:</h2>") + 1
+
+    # проверяем, что в первой строке результата указано 3
+    assert "3" in lines[idx]
+
+    # проверяем, что в блоке <pre> есть все три вершины
     assert "1" in text
+    assert "2" in text
     assert "3" in text
