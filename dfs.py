9711933d9ff0ac0b04d6746f9037e47ec665efb3
xmik <xmik2021@gmail.com>
2025-04-25 01:38:39 +0300

    fixed

--- dfs.py
+++ dfs.py
@@ -1,11 +1,9 @@
 def dfs(N,M,nums):
-    N, M = 2, 2
-    nums = [1, 2, 2, 1]
     U = {
+        1: []
     }
 
-
-    for _ in range(0,M+1,2):
+    for _ in range(0,M*2-1,2):
         a,b = (nums[_]), (nums[_+1])
         if U.get(a) == None:
             U[a] = []
@@ -16,8 +14,9 @@ def dfs(N,M,nums):
         if a!=b:
             U[b].append(a)
 
+
     visited = set()
-    next = set([2])
+    next = set([1])
 
     while(len(next)>0):
         x = next.pop()
@@ -28,3 +27,17 @@ def dfs(N,M,nums):
                 next.add(el)
 
     return f"{len(visited)}\n{' '.join(map(str, visited))}"
+
+def main():
+    N, M = map(int, input().split())
+    nums = []
+    for _ in range(M):
+        a, b = map(int, input().split())
+        nums.append(a)
+        nums.append(b)
+    print(dfs(N, M, nums))
+
+if __name__ == "__main__":
+    main()
+
+
